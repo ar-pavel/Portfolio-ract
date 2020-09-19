e86d6c1e30ce0af6ce88cddbe09da4a4ca5eeec8
ar-pavel <atikur1998@yahoo.com>
2020-09-19 16:48:53 +0600

    revision v1.1

--- src/components/CompetitiveProgramming.js
+++ src/components/CompetitiveProgramming.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const CompetitiveProgramming = () => (
   <>
-    <div className="page">
+    <div className="page" style={{ paddingTop: "100px" }}>
       <SEO title="Competitive Programming" />
       <div>
         <p className="icpc">
