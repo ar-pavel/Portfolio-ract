f75a850217b018c72b261a13438972ad8ce94d02
ar-pavel <atikur1998@yahoo.com>
2020-12-08 01:29:45 +0600

    typo fixed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,17 +16,17 @@ const Project = () => (
       />
        <Card
         name="RISC-V Simulator"
-        title="riscv"
+        title="RISC-V Simulator"
         description="32-bit single core based implementation of RISC-V ISS feature specifications and covers all permitted configurations."
-        live="https://submaxvlsm.herokuapp.com/"
-        src="https://github.com/ar-pavel/SUBMAX"
+        live="https://github.com/ar-pavel/RISC-V-Simulator"
+        src="https://github.com/ar-pavel/RISC-V-Simulator"
       />
       <Card
         name="WC-University-Portal"
         title="WC University Management System"
         description="WC University is an Imginary University and this project is to manage this university. There are 7 microservices for backend and a vaadin based front-end(under development) to maitain this system."
-        live="https://github.com/ar-pavel/RISC-V-Simulator"
-        src="https://github.com/ar-pavel/RISC-V-Simulator"
+        live="https://github.com/ar-pavel/WC-University-Portal"
+        src="https://github.com/ar-pavel/WC-University-Portal"
       />
       <Card
         name="Reversi-OthelloGame"
